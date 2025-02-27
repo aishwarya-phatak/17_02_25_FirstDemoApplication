890f4683007ed08473950f553623d7a789c65cc3
aishwarya <ashp17star@gmail.com>
2025-02-27 17:37:23 +0530

    Resources in Android and how to use them in Activity

--- app/src/main/java/com/bitcode/a17_02_25_firstdemoapplication/SecondActivity.java
+++ app/src/main/java/com/bitcode/a17_02_25_firstdemoapplication/SecondActivity.java
@@ -6,7 +6,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
